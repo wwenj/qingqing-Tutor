16a29500378ba474234e6fd40e9ee281b659818b
wwenj <man@wwenj.com>
2017-10-27 15:56:41 +0800

    2

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # qingqing-Tutor
 前端培训——轻轻家教前端页面
+[原网站](https://www.changingedu.com/beijing/r-)
--- js/main.js
+++ js/main.js
@@ -2,7 +2,27 @@ $(function(){
 /*index页*/
     /*导航栏input点击/失焦事件*/
     $('.inputon').on('click blur',function(){
-        $('.search-out').toggle();
+        $('.search-out').fadeToggle('fast');
+        if ($('.search').hasClass('searchClick')){
+            $('.search').removeClass('searchClick')
+        }else{
+            $('.search').addClass('searchClick')
+        }
     });
-
+    /*点击展开*/
+    $('.showMore').on('click',function(){
+        if($(this).find('img').hasClass('showMoreChang')){
+            $(this).find('img').removeClass('showMoreChang');
+            $(this).parent().find('p').addClass('hidden');
+        }else{
+            $(this).find('img').addClass('showMoreChang');
+            $(this).parent().find('p').removeClass('hidden');
+        }
+    });
+    for(var i=0;i<$('.type1').length;i++){
+        if($('.type1').eq(i).find('a').length<11){
+            $('.type1').eq(i).find('button').addClass('buttonHidden')
+            // console.log($('.type1').eq(i).find('button').html(""))
+        }
+    }
 });
